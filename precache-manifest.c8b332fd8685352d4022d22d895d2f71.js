c68730ad309b6e5b9670c55526edb828dbd15ce9
Ally Murray <allymurray88@gmail.com>
2020-03-31 19:32:13 +0100

    Updates

---
+++ precache-manifest.c8b332fd8685352d4022d22d895d2f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa1120e8fbeae42463223ae662ae8e8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1fa91efd7d9cb2520bc8",
+    "url": "/calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba6d6d7deab161756afe",
+    "url": "/calculator/static/js/2.ee2cd421.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.ee2cd421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa91efd7d9cb2520bc8",
+    "url": "/calculator/static/js/main.d580282e.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
